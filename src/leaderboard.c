cd9a784815b065eef89093212e301500287955af
Shaun Karran <shaun.karran@gmail.com>
2015-10-08 13:35:38 +1000

    Server now sends leaderboard to client. Sometimes errors with stack smashing.

--- src/leaderboard.c
+++ src/leaderboard.c
@@ -2,8 +2,6 @@
  *  CAB403 Assignment - Semester 2 2015
  *
  *  Created by Lachlan Cesca & Shaun Karran
- *
- *
  */
 
 #include "leaderboard.h"
@@ -11,24 +9,26 @@
 /*
  * Summary: Get/display leaderboard
  */
-void get_leaderboard(void){
-  for(int i=0;i<LEADERBOARD_LENGTH;i++){
-    if(user_scores[i].games_played==0){
-      /* First element is empty, then no information in leaderboard*/
-      if(i==0){
-        printf("=============================================================================\n\n");
-        printf("There is no information currently stored in the Leader Board. Try again later\n\n");
-        printf("=============================================================================\n\n");
-      }
-      return;
-    }else{
-      printf("==================================================\n\n");
-      printf("Player  - %s\nNumber of games won  - %d\nNumber of games played  - %d\n",user_scores[i].username,user_scores[i].games_won,user_scores[i].games_played);
-      printf("\n==================================================\n");
-    }
-  }
-}
+void get_leaderboard(char *str)
+{
+    char temp[100] = {0};
 
+    for (int i = 0; i < LEADERBOARD_LENGTH; i++) {
+        if (user_scores[i].games_played == 0 && i == 0) {
+            /* First element is empty, then no information in leaderboard. */
+            strcat(str, "\n=============================================================================\n");
+            strcat(str, "\nThere is no information currently stored in the Leader Board. Try again later\n");
+            strcat(str, "\n=============================================================================\n");
+            return;
+        } else {
+            strcat(str, "\n==================================================\n\n");
+            sprintf(temp, "Player  - %s\nNumber of games won  - %d\nNumber of games played  - %d\n",
+                    user_scores[i].username, user_scores[i].games_won, user_scores[i].games_played);
+            strcat(str, temp);
+            strcat(str, "\n==================================================\n");
+        }
+    }
+}
 
 /*
  * Summary: Set/update leaderboard and then sort list
@@ -37,29 +37,29 @@ void get_leaderboard(void){
  *        int win : whether the user won or not
  */
 void set_leaderboard(char *user,int win){
-  int user_pos = find_user(user);
-  if(user_pos!= -1){ /* If user saved*/
-      user_scores[user_pos].games_played += 1;
-      if(win==1){
-          user_scores[user_pos].games_won++;
-      }
+    int user_pos = find_user(user);
+    if(user_pos!= -1){ /* If user saved*/
+            user_scores[user_pos].games_played += 1;
+            if(win==1){
+                    user_scores[user_pos].games_won++;
+            }
 
-  }else{
-    if(users_full()==0){
-        //Save user in next spot
-        int save_pos = next_user_slot();
-        if(save_pos!=-1){
-          strcpy(user_scores[save_pos].username,user);
-          user_scores[save_pos].games_played = 1;
-          win==1 ? (user_scores[save_pos].games_won = 1) : (user_scores[save_pos].games_won = 0);
-        }
     }else{
-      printf("=======================================\n\n");
-      printf("Maximum number of saved users exceeded!\n\n");
-      printf("=======================================\n\n");
+        if(users_full()==0){
+                //Save user in next spot
+                int save_pos = next_user_slot();
+                if(save_pos!=-1){
+                    strcpy(user_scores[save_pos].username,user);
+                    user_scores[save_pos].games_played = 1;
+                    win==1 ? (user_scores[save_pos].games_won = 1) : (user_scores[save_pos].games_won = 0);
+                }
+        }else{
+            printf("=======================================\n\n");
+            printf("Maximum number of saved users exceeded!\n\n");
+            printf("=======================================\n\n");
+        }
     }
-  }
-  //TODO Resort List
+    //TODO Resort List
 }
 
 
@@ -75,12 +75,12 @@ void set_leaderboard(char *user,int win){
  *
  */
 int find_user(char *user){
-  for(int i=0;i<MAX_SAVED_USERS;i++){
-    if(strcmp(user,user_scores[i].username)==0){
-      return i;
+    for(int i=0;i<MAX_SAVED_USERS;i++){
+        if(strcmp(user,user_scores[i].username)==0){
+            return i;
+        }
     }
-  }
-  return -1;
+    return -1;
 }
 
 /*
@@ -92,11 +92,11 @@ int find_user(char *user){
  *
  */
 int leaderboard_full(void){
-    if(user_scores[LEADERBOARD_LENGTH-1].games_played==0){
-      return 0;
-    }else{
-      return 1;
-    }
+        if(user_scores[LEADERBOARD_LENGTH-1].games_played==0){
+            return 0;
+        }else{
+            return 1;
+        }
 }
 
 
@@ -109,11 +109,11 @@ int leaderboard_full(void){
  *
  */
 int users_full(void){
-    if(user_scores[MAX_SAVED_USERS-1].games_played==0){
-      return 0;
-    }else{
-      return 1;
-    }
+        if(user_scores[MAX_SAVED_USERS-1].games_played==0){
+            return 0;
+        }else{
+            return 1;
+        }
 }
 
 
@@ -126,10 +126,10 @@ int users_full(void){
  *
  */
 int next_user_slot(void){
-  for(int i=0;i<MAX_SAVED_USERS;i++){
-    if(strcmp("",user_scores[i].username)==0){
-      return i;
+    for(int i=0;i<MAX_SAVED_USERS;i++){
+        if(strcmp("",user_scores[i].username)==0){
+            return i;
+        }
     }
-  }
-  return -1;
+    return -1;
 }
