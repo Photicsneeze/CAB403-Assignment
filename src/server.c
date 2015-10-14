4a35b50bd97134e9a3e55f705476b09f4d0d6947
Shaun Karran <shaun.karran@gmail.com>
2015-10-14 10:17:57 +1000

    Created client data type to hold information relevant to individual clients. Modified functions to take client as argument. Mostly to be able to send/receive data from the right client (sock_fd). Moved authenticate function to server and changes name to authenticate_user.
    Still getting error "accept: bad address" when removing call to authenticate from main.

--- src/server.c
+++ src/server.c
@@ -11,14 +11,17 @@
 int main(int argc, char *argv[])
 {
     char        *port;
-    int         ret;                    /* Return value for recv() */
-    addrinfo    *addr;                  /* Contains internet address information of server */
-    char        recv_buf[BUF_SIZE];     /* Buffer to store data received from client */
-    char        send_buf[BUF_SIZE];     /* Buffer to store data to send to the client */
-    char        username[8];
-    char        password[6];
-    int         menu_selection;
-    pthread_t   client1_thread;
+    int         ret;                        /* Return value for recv() */
+    addrinfo    *addr;                      /* Contains internet address information of server */
+    char        recv_buf[BUF_SIZE];         /* Buffer to store data received from client */
+    char        send_buf[BUF_SIZE];         /* Buffer to store data to send to the client */
+    char        username[USERNAME_LENGTH];
+    char        password[PASSWORD_LENGTH];
+    pthread_t   client_threads[10];
+    pthread_t   client_thread;
+    int         num_clients_connected = 0;  /* Number of clients currently connected to server. */
+    int         client_id = 0;              /* ID to give client. Increment for every connection. */
+    Client_Data current_client;
 
     /* Check the user provided the correct arguments. If no port provided, use default. */
     if (argc < 2) {
@@ -33,9 +36,9 @@ int main(int argc, char *argv[])
 
     signal(SIGINT, shutdown_server); /* Tell the program which function to call when ctrl + c is pressed. */
 
-    sock_fd = create_passive_socket(port, addr);
-
     leaderboard = create_leaderboard();
+
+    passive_sock_fd = create_passive_socket(port, addr);
 
     /* Main server loop. Accept incomming connections, send/recv data, close connection. */
     for (;;) {
@@ -43,42 +46,38 @@ int main(int argc, char *argv[])
          * Accept is a blocking function and will wait for a connection if none available.
          */
         printf("\nWaiting for connection...\n");
-        if ((new_sock_fd = accept(sock_fd, addr->ai_addr, &addr->ai_addrlen)) == -1) {
+        global_client.sock_fd = accept(passive_sock_fd, addr->ai_addr, &addr->ai_addrlen);
+        if (global_client.sock_fd == -1) {
             perror("accept");
             continue; /* Failed to accept connection, continue to start on main loop. */
         }
         printf("Connection accepted.\n\n");
 
-        client1_data.id = 1;
-        client1_data.sock_fd = new_sock_fd;
-        client1_data.connected = true;
+        global_client.id = ++client_id;
+        global_client.connected = true;
 
-        if (pthread_create(&client1_thread, NULL, handle_client, (void *) &client1_data) != 0) {
+        if (pthread_create(&client_thread, NULL, handle_client, (void *) &global_client) != 0) {
             perror("pthread_create");
             exit(EXIT_FAILURE);
         }
 
-        if (pthread_join(client1_thread, NULL) != 0) {
-            perror("pthread_join");
-            exit(EXIT_FAILURE);
-        }
+        num_clients_connected++;
 
-        if (0) {
-            authenticate(username, password);
-        }
+        authenticate_user(username, password);
     }
 
     /* Close socket. */
-    close(sock_fd);
+    close(passive_sock_fd);
 
     exit(EXIT_SUCCESS);
 }
 
 void* handle_client(void *client_data)
 {
-    char            username[10];
-    char            password[6];
+    // char            username[10];
+    // char            password[6];
     int             menu_selection;
+    bool            win;
     Client_Data     *client;
 
     client = (Client_Data *) client_data;
@@ -87,12 +86,12 @@ void* handle_client(void *client_data)
     printf("Sending welcome message to client %d...\n", client->id);
     write_to_client(client->sock_fd, WELCOME_MESSAGE);
 
-    get_username(username);
-    get_password(password);
-    //strcpy(username, "Maolin");
-    //strcpy(password, "111111");
+    get_username(client);
+    get_password(client);
+    //strcpy(client->username, "Maolin");
+    //strcpy(client->password, "111111");
 
-    if (!authenticate(username, password)) {
+    if (!authenticate_user(client->username, client->password)) {
         printf("Sending thread auth failed message to client %d...\n", client->id);
         write_to_client(client->sock_fd, AUTH_FAILED);
 
@@ -104,15 +103,15 @@ void* handle_client(void *client_data)
         printf("Sending main menu to client %d...\n", client->id);
         write_to_client(client->sock_fd, MAIN_MENU);
 
-        menu_selection = get_menu_selection();
+        menu_selection = get_menu_selection(client);
 
         switch (menu_selection) {
             case PLAY_HANGMAN:;
-                bool win = play_hangman(username);
-                update_score(leaderboard, username, win);
+                win = play_hangman(client);
+                update_score(leaderboard, client->username, win);
                 break;
             case SHOW_LEADERBOARD:
-                send_leaderboard(leaderboard);
+                send_leaderboard(leaderboard, client);
                 break;
             case QUIT:
                 disconnect_client(client->sock_fd);
@@ -124,7 +123,7 @@ void* handle_client(void *client_data)
     pthread_exit(NULL);
 }
 
-bool play_hangman(char *user) {
+bool play_hangman(Client_Data *client) {
     Game game;
     char guess[2];                  /* Not sure why this had to be 2 char array. Breaks as a char. */
     char game_interface[BUF_SIZE];
@@ -138,30 +137,31 @@ bool play_hangman(char *user) {
         memset(game_interface, 0, sizeof(game_interface)); /* Clear the interface from previous round. */
         display_game(&game, game_interface);
         printf("Sending game interface...\n");
-        write_to_client(new_sock_fd, game_interface);
+        write_to_client(client->sock_fd, game_interface);
 
         if (check_complete(&game)) { /* Win */
             win = true;
             sprintf(game_over_message,
-                    "\n\nGame Over\nWell done %s! You won this round of Hangman!", user);
+                    "\n\nGame Over\nWell done %s! You won this round of Hangman!", client->username);
 
             printf("Sending win message...\n");
-            write_to_client(new_sock_fd, game_over_message);
+            write_to_client(client->sock_fd, game_over_message);
             return win;
         }
 
         if (game.guess_count >= game.number_guesses) { /* Lose */
             sprintf(game_over_message,
-                    "\n\nGame Over\nBad luck %s! You have run out of guesses. The Hangman got you!", user);
+                    "\n\nGame Over\nBad luck %s! You have run out of guesses. The Hangman got you!",
+                    client->username);
 
             printf("Sending lose message...\n");
-            write_to_client(new_sock_fd, game_over_message);
+            write_to_client(client->sock_fd, game_over_message);
             return win;
         }
 
         printf("Waiting for guess from client...\n");
         /* Have to receive 2 bytes otherwise it seems to read the enter key character on next loop. */
-        if (read(new_sock_fd, guess, BUF_SIZE) == -1) {
+        if (read(client->sock_fd, guess, BUF_SIZE) == -1) {
             perror("read");
             exit(EXIT_FAILURE);
         }
@@ -173,54 +173,79 @@ bool play_hangman(char *user) {
     }
 }
 
-void send_leaderboard(Leaderboard *leaderboard) {
+void send_leaderboard(Leaderboard *leaderboard, Client_Data *client) {
     char score_str[BUF_SIZE];
 
     for (int i = 0; i < leaderboard->num_scores; i++) {
         memset(score_str, 0, BUF_SIZE);
         score_to_string(score_str, leaderboard->entries[i]);
-        write_to_client(new_sock_fd, score_str);
+        write_to_client(client->sock_fd, score_str);
     }
 }
 
-void get_username(char *username)
+void get_username(Client_Data *client)
 {
     /* Prompt for username. */
     printf("Sending username prompt...\n");
-    write_to_client(new_sock_fd, USERNAME_PROMPT);
+    write_to_client(client->sock_fd, USERNAME_PROMPT);
 
     printf("Waiting for username from client...\n");
-    if (read(new_sock_fd, username, BUF_SIZE) == -1) {
+    if (read(client->sock_fd, client->username, BUF_SIZE) == -1) {
         perror("read");
         exit(EXIT_FAILURE);
     }
     printf("Username received.\n");
 }
 
-void get_password(char *password)
+void get_password(Client_Data *client)
 {
     /* Prompt for password. */
     printf("Sending password prompt...\n");
-    write_to_client(new_sock_fd, PASSWORD_PROMPT);
+    write_to_client(client->sock_fd, PASSWORD_PROMPT);
 
     printf("Waiting for password from client...\n");
-    if (read(new_sock_fd, password, BUF_SIZE) == -1) {
+    if (read(client->sock_fd, client->password, BUF_SIZE) == -1) {
         perror("read");
         exit(EXIT_FAILURE);
     }
     printf("Password received.\n");
 }
 
-int get_menu_selection()
+bool authenticate_user(char *username, char *password)
+{
+    FILE *file;
+    char file_username[USERNAME_LENGTH];
+    char file_password[PASSWORD_LENGTH];
+
+    /* !!! REMOVE THE bin/ FROM FILE NAME BEFOER SUBMISSION. Only there for convenience while testing. */
+    if ((file = fopen("bin/Authentication.txt", "r")) == NULL) {
+        perror("fopen");
+        return false;
+    }
+
+    while (fgetc(file) != '\n'); /* Move past the column titles in the file. */
+
+    /* Each line in the file contains the username and password separated by a space. */
+    while (fscanf(file, "%s %s\n", file_username, file_password) > 0) {
+        /* Check if the username matches one in the file, and if the password matches for that username. */
+        if (strcmp(file_username, username) == 0 && strcmp(file_password, password) == 0) {
+            return true;
+        }
+    }
+
+    return false; /* No matches found. */
+}
+
+int get_menu_selection(Client_Data *client)
 {
     char selection_str[BUF_SIZE];
 
     /* Prompt for main menu. */
     printf("Sending menu selection prompt...\n");
-    write_to_client(new_sock_fd, MENU_PROMPT);
+    write_to_client(client->sock_fd, MENU_PROMPT);
 
     printf("Waiting for menu selection from client...\n");
-    if (read(new_sock_fd, selection_str, BUF_SIZE) == -1) {
+    if (read(client->sock_fd, selection_str, BUF_SIZE) == -1) {
         perror("read");
         exit(EXIT_FAILURE);
     }
@@ -297,7 +322,9 @@ void disconnect_client(int sock_fd)
 
 void shutdown_server(int sig)
 {
-    disconnect_client(new_sock_fd);
+    disconnect_client(global_client.sock_fd);
     free_leaderboard(leaderboard);
+    close(passive_sock_fd);
+
     exit(EXIT_SUCCESS);
 }
